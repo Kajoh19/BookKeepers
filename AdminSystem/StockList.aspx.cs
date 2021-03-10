c82beff2418c78b2e963c5b129a96e1a22296743
Samsam Afjar <P2561845@my365.dmu.ac.uk>
2021-03-10 17:35:31 +0000

    Creating the Collection class

--- AdminSystem/StockList.aspx.cs
+++ AdminSystem/StockList.aspx.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if(IsPostBack == false)
+        {
+            //update the list box
+            DisplayStock();
+        }
+         
     }
+    void DisplayStock()
+        {
+            clsStockCollection Stock = new clsStockCollection();
+            lstStockList.DataSource = Stock.StockList;
+            lstStockList.DataValueField = "BookId";
+            lstStockList.DataTextField = "Description";
+            lstStockList.DataBind();
+        }
 }
