4bcbdc681c12dc583cfa7a8118f8bf02d9e24a70
P2544876 <P2544876@GWHXX679WIN008W>
2021-04-23 15:58:41 +0100

    Added add,edit,delete and filter methods to all layers.

--- AdminSystem/CustomerList.aspx
+++ AdminSystem/CustomerList.aspx
@@ -5,11 +5,38 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 1003px;
+            height: 450px;
+            position: absolute;
+            left: 10px;
+            top: 15px;
+            bottom: 127px;
+        }
+    </style>
 </head>
 <body>
-    <form id="form1" runat="server">
+    <form id="form1" runat="server" class="auto-style1">
         <div>
         </div>
+        <asp:ListBox ID="lstCustomers" runat="server" Height="258px" Width="381px"></asp:ListBox>
+        <p>
+            <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click1" />
+            <asp:Button ID="btnEdit" runat="server" OnClick="Button1_Click" Text="Edit" />
+            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click1" />
+        </p>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Enter an email"></asp:Label>
+            <asp:TextBox ID="txtFilter" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="btnApply" runat="server" OnClick="btnApply_Click" Text="Apply" />
+            <asp:Button ID="btnClear" runat="server" OnClick="btnCancel_Click" Text="Clear" />
+        </p>
+        <p>
+        <asp:Label ID="lblError" runat="server"></asp:Label>
+        </p>
     </form>
 </body>
 </html>
---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ClassLibrary
+{
+    public class clsCustomerCollection
+    {
+        //private data member for the list
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+        //private data member thisCustomer
+        clsCustomer mThisCustomer = new clsCustomer();
+
+        //constructor for the class
+        public clsCustomerCollection()
+        {
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure 
+            DB.Execute("sproc_tblCustomer_SelectAll");
+            //populate the array list with the data table 
+            PopulateArray(DB);
+
+        }
+
+        public List<clsCustomer> CustomerList
+        {
+            get
+            {
+                //return the private data
+                return mCustomerList;
+            }
+            set
+            {
+                //set the private data
+                mCustomerList = value;
+            }
+        }
+
+        public clsCustomer ThisCustomer
+        {
+            get
+            {
+                //return the private data 
+                return mThisCustomer;
+            }
+            set
+            {
+                //set the private data 
+                mThisCustomer = value;
+            }
+
+
+        }
+
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mCustomerList.Count;
+            }
+            set { }
+
+        }
+
+        public int Add()
+        {
+            //adds a new record to the database based on the values of thisCustomer
+            //connect to the database 
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure 
+            DB.AddParameter("@Email", mThisCustomer.Email);
+            DB.AddParameter("@UserPassword", mThisCustomer.UserPassword);
+            DB.AddParameter("@DateAdded", mThisCustomer.DateAdded);
+            DB.AddParameter("@Budget", mThisCustomer.Budget);
+            DB.AddParameter("@Active", mThisCustomer.Active);
+            //execute the query returning the primary key value 
+            return DB.Execute("sproc_tblCustomer_Insert");
+
+
+        }
+
+        public void Delete()
+        {
+            //declare the record pointed to by thisCustomer 
+            //connect to the database 
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure 
+            DB.AddParameter("@CustomerId", mThisCustomer.CustomerId);
+            //execute the stored procedure 
+            DB.Execute("sproc_tblCustomer_Delete");
+        }
+
+        public void Update()
+        {
+
+            //update an existing record on the values of thisCustomer
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@CustomerId", mThisCustomer.CustomerId);
+            DB.AddParameter("@Email", mThisCustomer.Email);
+            DB.AddParameter("@UserPassword", mThisCustomer.UserPassword);
+            DB.AddParameter("@DateAdded", mThisCustomer.DateAdded);
+            DB.AddParameter("@Budget", mThisCustomer.Budget);
+            DB.AddParameter("@Active", mThisCustomer.Active);
+            //execute the stored procedure 
+            DB.Execute("sproc_tblCustomer_Update");
+        }
+
+        public void ReportByEmail(string Email)
+        {
+            //filters the record based on a full or partial Email
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //send the email parameter to the database 
+            DB.AddParameter("@Email", Email);
+            //execute the stored procedure 
+            DB.Execute("sproc_tblCustomer_FilterByEmail");
+            //populate the array list with the data table
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            //populates the array list based on the data table in the parameter DB
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount;
+            //get the count of the records 
+            RecordCount = DB.Count;
+            //clear the private array list
+            mCustomerList = new List<clsCustomer>();
+            //while there are records to process 
+            while (Index < RecordCount)
+            {
+                //create a blank customer
+                clsCustomer ACustomer = new clsCustomer();
+                //read in the fields from the current record
+                ACustomer.Email = Convert.ToString(DB.DataTable.Rows[Index]["Email"]);
+                ACustomer.UserPassword = Convert.ToString(DB.DataTable.Rows[Index]["UserPassword"]);
+                ACustomer.Budget = Convert.ToDecimal(DB.DataTable.Rows[Index]["Budget"]);
+                ACustomer.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+                ACustomer.CustomerId = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerId"]);
+                //add the records to the private data memeber 
+                mCustomerList.Add(ACustomer);
+                //point at the next record 
+                Index++;
+
+            }
+        }
+    }
+}
